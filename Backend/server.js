10477408635a3e6b851cf133c6985d8be48af1cf
Anshi <anshikhatri37@gmail.com>
2026-02-14 23:26:32 -0700

    added route domains and required constants, now we just need to implement the functions

--- Backend/server.js
+++ Backend/server.js
@@ -1,25 +1,20 @@
 const express = require('express');
 const app = express();
 const db = require('./config/firebase');
-const accountstuff = require('./accountroutes');
-const stickystuff = require('./stickyroutes'); 
+const accountstuff = require('./routes/accountroutes'); // functions returned here 
+const stickystuff = require('./routes/stickyroutes'); // return the functions here
 
 // serves as a "main page " for the backend 
 
 app.use(express.json());
 
-app.use('/account', accountstuff)
+// these are the different domains our backend will be under split into two sections,
+// stuff for accounts and the stickynote stuff 
+
+app.use('/account', accountstuff) // defines the api url to use in the frontend , make naming conventions more simple
 app.use('/notes', stickystuff)
 
-// app.get('/', async (req, res) => { // root route confirmation message 
-//   try {
-//     const snapshot = await db.collection('test').get(); // this should return nothing because we dont have anything 
-//     res.send('Backend is runninhg');
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).send(error.message);
-//   }
-// });
+
 
 app.listen(3000, () => {
   console.log('Server running on port 3000');
