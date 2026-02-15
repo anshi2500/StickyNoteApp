a345a5d07f8fe4dc4e9d447939c9cee2a1193518
Anshi <anshikhatri37@gmail.com>
2026-02-14 20:44:32 -0700

    Test edit to server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,20 +1,24 @@
 const express = require('express');
 const app = express();
-const db = require('./config/firebase')
+const db = require('./config/firebase');
+const accountstuff = require('./accountstuff');
+const stickystuff = require('./stickytuff'); 
 
+// serves as a "main page " for the backend 
 
 app.use(express.json());
 
+// app.get('/', async (req, res) => { // root route confirmation message 
+//   try {
+//     const snapshot = await db.collection('test').get(); // this should return nothing because we dont have anything 
+//     res.send('Backend is runninhg');
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).send(error.message);
+//   }
+// });
 
-app.get('/', async (req, res) => {
-  try {
-    const snapshot = await db.collection('test').get(); // this should return nothing because we dont have anything 
-    res.send('Firestore read worked');
-  } catch (error) {
-    console.error(error);
-    res.status(500).send(error.message);
-  }
-});
+
 
 
 app.listen(3000, () => {
