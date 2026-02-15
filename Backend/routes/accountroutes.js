bfd083217981baf16be692c0e6164ad1d53fe7fb
Anshi <anshikhatri37@gmail.com>
2026-02-14 23:31:39 -0700

    Routes added

---
+++ Backend/routes/accountroutes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const db = require('./config/firebase');
+
+// create an account
+
+
+
+// authenticate account
+
+
+
+
+// delete account??
+
+
+
+
+
+module.exports = router; 
---
+++ Backend/routes/stickyroutes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const db = require('./config/firebase');
+
+// delete a sticky note
+
+
+
+// create a sticky note
+
+
+
+
+// fetch all sticky notes 
+
+
+
+
+
+
+module.exports = router; 
